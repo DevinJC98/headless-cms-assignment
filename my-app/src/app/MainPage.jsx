1eba1c1424afa1dd93f44eda7afb35671713ab05
DevinJC98 <devinjc98@hotmail.com>
2025-02-28 22:09:19 -0700

    Notes, Styling, and Responsiveness

--- my-app/src/app/MainPage.jsx
+++ my-app/src/app/MainPage.jsx
@@ -1,22 +1,30 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
+import "./globals.css";
 
 export default function MainPage() {
   const [assignment, setAssignment] = useState([]);
+
+  //I needed to add this second use state to get the image working.
   const [duck, setDuck] = useState([]);
+
+  //Same goes for this const. when pulled from the api the img url did not contain the localhost part of the url.
   const urlforimg = "http://localhost:1337";
 
   useEffect(() => {
+    //Here I am fetching the api and sending an error message if the fetch fails.
     async function getApi() {
       try {
-        const res = await fetch(
+        const api = await fetch(
+          //The image wasn't loaded into the api by default so I needed to populate the database with mainimage.
           "http://localhost:1337/api/assignment?populate=mainimage"
         );
-        const content = await res.json();
+        const content = await api.json();
         setAssignment(content.data);
+        //populating the database returned multiple attributes so i had to dig deeper to fetch the imgage url
         setDuck(content.data.mainimage);
+        //failed fetch error message
       } catch (err) {
         console.error("Something Went Wrong", err);
       }
@@ -24,11 +32,14 @@ export default function MainPage() {
     getApi();
   }, []);
 
+  //I used concatination to merge the local host with the image url
   return (
     <main>
       <img src={urlforimg + duck.url} alt="The Duck" width="500" height="500" />
-      <h1>{assignment.headingtext}</h1>
-      <p>{assignment.bodytext}</p>
+      <div id="textHolder">
+        <h1>{assignment.headingtext}</h1>
+        <p>{assignment.bodytext}</p>
+      </div>
     </main>
   );
 }
